b2f7fa5aa1e8626ce9eabc76be4b199a5e150897
alec-r-reynolds <alechasrito@gmail.com>
2020-08-02 00:36:42 -0600

    Library View, Edit Book, Delete

--- src/components/create-book.js
+++ src/components/create-book.js
@@ -1,11 +1,127 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
 
 export default class CreateBook extends Component {
+  constructor(props) {
+    super(props);
+
+    this.onChangeAuthor = this.onChangeAuthor.bind(this);
+    this.onChangeGenre = this.onChangeGenre.bind(this);
+    this.onChangeTitle = this.onChangeTitle.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+    
+    this.state = {
+      author: '',
+      genre: '',
+      title: '',
+      read: false,
+    
+    }
+  }
+
+  componentDidMount() {
+    this.setState({
+      
+    })
+  }
+
+  onChangeAuthor(event) {
+    this.setState({
+      author: event.target.value
+    });
+  }
+
+  onChangeTitle(event) {
+    this.setState({
+      title: event.target.value
+    });
+  }
+
+  onChangeGenre(event) {
+    this.setState({
+      genre: event.target.value
+    });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const book = {
+      author: this.state.author,
+      genre: this.state.genre,
+      title: this.state.title,
+      read: this.state.read
+    }
+
+    console.log(book);
+
+    axios.post('http://localhost:4000/api/books', book).then(res => console.log(res.data));
+
+    window.location = '/';
+  }
+
+
+
+  
+
   render() {
     return (
       <div>
-        <p>Create Book</p>
-      </div>
+      <h3>Create New Book</h3>
+      <form onSubmit={this.onSubmit}>
+        {/* <div className="form-group"> 
+          <label>Title: </label>
+          <select ref="userInput"
+              required
+              className="form-control"
+              value={this.state.title}
+              onChange={this.onChangeTitle}>
+              {
+                this.state.users.map(function(user) {
+                  return <option 
+                    key={user}
+                    value={user}>{user}
+                    </option>;
+                })
+              }
+          </select>
+        </div> */}
+        <div className="form-group"> 
+          <label>Title: </label>
+          <input  type="text"
+              required
+              className="form-control"
+              value={this.state.title}
+              onChange={this.onChangeTitle}
+              />
+        </div>
+        <div className="form-group">
+          <label>Author: </label>
+          <input 
+              required
+              type="text" 
+              className="form-control"
+              value={this.state.author}
+              onChange={this.onChangeAuthor}
+              />
+        </div>
+        <div className="form-group">
+          <label>Genre: </label>
+          <input 
+              required
+              type="text" 
+              className="form-control"
+              value={this.state.genre}
+              onChange={this.onChangeGenre}
+              />
+        </div>
+
+        <div className="form-group">
+          <input type="submit" value="Create New Book" className="btn btn-primary" />
+        </div>
+      </form>
+    </div>
     )
   }
 }
--- src/components/library-view.js
+++ src/components/library-view.js
@@ -1,10 +1,74 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+import axios from 'axios';
+
+const Book = props => (
+  <tr>
+    <td>{props.book.title}</td>
+    <td>{props.book.author}</td>
+    <td>{props.book.genre}</td>
+    <td>
+      <Link to = {"/edit/" + props.book._id}>edit</Link> | <a href = "#" onClick={() => {props.deleteBook(props.book._id)}}>delete</a>
+    </td>
+  </tr>
+)
 
 export default class LibraryView extends Component {
+  constructor(props) {
+    super(props);
+
+    this.deleteBook = this.deleteBook.bind(this);
+
+    this.state = {
+      books: [],
+    }
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:4000/api/books')
+      .then(response => {
+        this.setState({
+          books: response.data
+        })
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
+
+  deleteBook(id) {
+    axios.delete('http://localhost:4000/api/books/' + id)
+    .then(response => console.log(response.data));
+
+    this.setState({
+      books: this.state.books.filter(element => element._id !== id)
+    })
+  }
+
+  booksList() {
+    return this.state.books.map(currentbook => {
+      return <Book book = {currentbook} deleteBook = {this.deleteBook} key={currentbook._id}/>;
+    })
+  }
+
+
   render() {
     return (
       <div>
-        <p>Library LibraryView</p>
+        <h3>Library View</h3>
+        <table className ="table">
+          <thead className="thead-light">
+            <tr>
+              <th>Title</th>
+              <th>Author</th>
+              <th>Genre</th>
+              <th>Read</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.booksList()}
+          </tbody>
+        </table>
       </div>
     )
   }
