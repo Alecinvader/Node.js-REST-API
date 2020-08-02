5fc879349232de2d348d60b0b4c834d980ca6382
alec-r-reynolds <alechasrito@gmail.com>
2020-08-02 14:06:23 -0600

    change res to response

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ if(process.env.ENV === 'Test'){
   app.use(express.static(path.join(__dirname, 'build')))
 
   app.get('*', (request, response) => {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')); // relative path
+    response.sendFile(path.join(__dirname, 'client', 'build', 'index.html')); // relative path
   })
 
 
