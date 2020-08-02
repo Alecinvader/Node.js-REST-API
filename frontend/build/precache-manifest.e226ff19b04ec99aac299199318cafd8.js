a4488c08ecaa325f45b1419abe531feecc1f1f52
alec-r-reynolds <alechasrito@gmail.com>
2020-08-02 11:41:04 -0600

    Move directories

---
+++ frontend/build/precache-manifest.e226ff19b04ec99aac299199318cafd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d86d6f98bd5806d9fb77b374052bcf0a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e81db6b062dd8c5501c",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "7e81db6b062dd8c5501c",
+    "url": "/static/js/2.f53c8f44.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f53c8f44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "374aa51ccff13b42f702",
+    "url": "/static/js/main.c3be98f8.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
