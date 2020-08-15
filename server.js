6d3adc9050c1e90f717c8f736f7af55502115ea6
alec-r-reynolds <alechasrito@gmail.com>
2020-08-14 18:32:47 -0600

    Changed keys added herkou config

--- server.js
+++ server.js
@@ -1,41 +1,37 @@
-
-const mongoose = require('mongoose');
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
-const cors = require('cors');
+const mongoose = require("mongoose");
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const cors = require("cors");
 
 // Importing files
-const Book = require('./models/bookModel');
-const routes = require('./routes')(Book);
+const Book = require("./models/bookModel");
+const routes = require("./routes")(Book);
 
 const app = express();
 let port = process.env.PORT || 4000;
 
-mongoose.connect('mongodb+srv://Alec-Reynolds:Admin-Password@cluster0.tc5tp.mongodb.net/bookAPI?retryWrites=true&w=majority', {useNewUrlParser: true});
-
+mongoose.connect(
+  `mongodb+srv://Alec-Reynolds:${process.env.MONGODB_KEY}@cluster0.tc5tp.mongodb.net/bookAPI?retryWrites=true&w=majority`,
+  { useNewUrlParser: true }
+);
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
-app.use('/', routes);
-
+app.use("/", routes);
 
 // app.get('/', (req, res) => {
 //   res.send('Welcome to my Nodemon API!');
 // });
 
-if (process.env.NODE_ENV === 'production') {
-
-  app.use(express.static(path.join(__dirname, 'client/build')))
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
 
   // app.get('/*', (request, response) => {
-  //   response.sendFile(path.join(__dirname, 'client', 'build', 'index.html')); 
+  //   response.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
   // })
-
 }
-
-
 
 app.listen(port, () => {
   console.log(`Running on port ${port}`);
